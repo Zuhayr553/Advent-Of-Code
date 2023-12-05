efc9f158355b9de44c8d9bd1ec85c3827b3140e2
Zuhayr553 <zuhayrnoor444@gmail.com>
2023-12-05 15:30:54 +0100

    Advent Of Code Day - 01

--- solutions/day01.dart
+++ solutions/day01.dart
@@ -1,39 +1,57 @@
 import '../utils/index.dart';
 
-/// Every day should extend [GenericDay] to have access to the corresponding
-/// input and a common interface.
-///
-/// Naming convention is set to pad any single-digit day with `0` to have proper
-/// ordering of files and correct mapping between input for days and the day
-/// files.
 class Day01 extends GenericDay {
-  // call the superclass with an integer == today´s day
   Day01() : super(1);
 
-  /// The [InputUtil] can be accessed through the superclass variable `input`. \
-  /// There are several methods in that class that parse the input in different
-  /// ways, an example is given below
-  ///
-  /// The return type of this is `dynamic` for [GenericDay], so you can decide
-  /// on a day-to-day basis what this function should return.
   @override
-  List<int> parseInput() {
-    final lines = input.getPerLine();
-    // exemplary usage of ParseUtil class
-    return ParseUtil.stringListToIntList(lines);
+  List<String> parseInput() {
+    return input.getPerLine();
   }
 
-  /// The `solvePartX` methods always return a int, the puzzle solution. This
-  /// solution will be printed in main.
   @override
   int solvePart1() {
-    // TODO implement
-    return 0;
+    return parseInput()
+            .map((e) => e.replaceAll(RegExp(r'\D'), ' '))
+            .map((e) => e.replaceAll(' ', ''))
+            .map(handleCases)
+            .map((e) => int.tryParse(e))
+            .reduce((value, element) => value! + element!) ??
+        0;
   }
 
   @override
   int solvePart2() {
-    // TODO implement
-    return 0;
+    return parseInput()
+            .map(stringToNumber)
+            .map((e) => e.replaceAll(RegExp(r'\D'), ' '))
+            .map((e) => e.replaceAll(' ', ''))
+            .map(handleCases)
+            .map((e) => int.tryParse(e))
+            .reduce((value, element) => value! + element!) ??
+        0;
+  }
+
+  String handleCases(String e) {
+    if (e.length > 1) {
+      return e = e[0] + e[e.length - 1];
+    } else if (e.length == 1) {
+      return e = e[0] + e[0];
+    } else {
+      return throw Exception('Empty Line');
+    }
+  }
+
+  String stringToNumber(String named) {
+    return named
+        .replaceAll('one', 'o1e')
+        .replaceAll('two', 't2o')
+        .replaceAll('three', 't3e')
+        .replaceAll('four', 'f4r')
+        .replaceAll('five', 'f5e')
+        .replaceAll('six', 's6x')
+        .replaceAll('seven', 's7n')
+        .replaceAll('eight', 'e8t')
+        .replaceAll('nine', 'n9e')
+        .replaceAll('zero', 'z0e');
   }
 }
